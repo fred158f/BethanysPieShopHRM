c71e7d7b5b7f374f0e37ec191a01329fd1e18355
Frederik Christoffer Sørensen <frederikcsoerensen@gmail.com>
2023-02-20 12:54:43 +0100

    AddedCalendar

---
+++ BethanysPieShopHRM.Shared/Models/DayEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Shared.Models
+{
+    public class DayEvent
+    {
+        public int DayEventId { get; set; }
+        public string Note { get; set; }
+        public DateTime EventDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string DateValue { get; set; }
+        public string DayName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ BethanysPieShopHRM.Shared/Models/WeekClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Shared.Models
+{
+    public class WeekClass
+    {
+        public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+    }
+}
